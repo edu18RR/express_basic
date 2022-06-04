9e1979e2fb2fd93337881ed350c175b54c14263a
edu18RR <edu.rosas.16@gmail.com>
2022-06-04 07:39:27 -0500

    P8 agragando route /explorers/:explorer y recibiendo req.params

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000 // localhost:3000
 
+//localhost:3000/
 app.get('/', (req, res) => {
     res.send('Edu Rangel Bienvenido!')
     })
@@ -13,13 +14,23 @@ app.get('/launchx', (req, res) => {
     })
 
 app.get('/node/', (req, res) => {
-    const explorer1 = {id: 1, name: "exlorer"}
+    const explorer1 = {id: 1, name: "exlorer"} //{objeto de java script}
     const explorer2 = {id: 2, name: "edu"}
     const explorer3 = {id: 3, name: "Vero"}
     const explorer4 = {id: 4, name: "Emm"}
     res.send([explorer1, explorer2, explorer3, explorer4]) //para reflejarse en el servidor es necesario correr node app.js
     })
 
+    // Query Params: Recibir parámetros por la url
+// http://localhost:3000/explorers/edu
+// req.params = {"explorerName":"edu"}
+app.get('/explorers/:explorer', (req, res) => {
+    console.log(req.params)
+    res.send(req.params)
+})
+
+
+// Con esto inicializamos esta app
 app.listen(port, () => {
     console.log('Server is running')
 }) //control C para parar o servidor
